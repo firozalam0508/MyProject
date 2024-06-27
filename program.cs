a16f9041f43e2204c5243d3f9808d4863aba06e3
root <root@ip-172-31-6-203.us-east-2.compute.internal>
2024-06-27 11:38:21 +0000

    subtract prog

--- program.cs
+++ program.cs
@@ -6,6 +6,13 @@ Console.WriteLine(sum);
 
 int x = 5;
 int y = 6;
+<<<<<<< Updated upstream
 int mult= x * y;
 Console.WriteLine(mult);
+=======
+int sub = x - y;
+Console.WriteLine(sub);
+
+
+>>>>>>> Stashed changes
 
